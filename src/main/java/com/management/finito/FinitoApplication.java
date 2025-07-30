9c5d64ea03be0249746011c2ea1c5d4ccb822750
joan.santos <joansantosreis@gmail.com>
2025-07-30 15:46:58 -0300

    Primeiro commit

---
+++ src/main/java/com/management/finito/FinitoApplication.java
@@ -0,0 +1,13 @@
+package com.management.finito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/management/finito/FinitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.finito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
